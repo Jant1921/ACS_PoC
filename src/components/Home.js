aebd15f46dac8d638de0a8e755bc4931d4fae9e4
Jose Ruiz <jruizj6@hotmail.com>
2017-03-04 00:40:06 -0600

    copy images working on localhost only

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router';
+
 export default class Home extends React.Component{
+	
+	loadImages(pEvent){
+		let files = document.getElementById('files_input').value;
+	    if (files) {
+	        console.log(files);
+	        //console.log("Name: " + file.name + "\n" + "Last Modified Date :" + file.lastModifiedDate);
+	    }else{
+	    	console.log('no file');
+	    }
+	}
+	
 	render(){
 		return(
 			<div className="home__container">
-				<h1> Home Page </h1>
+				<h1> Prepocesamiento de Imágenes </h1>
+				<div>
+					
+					<form action="http://localhost:3001" method="post" encType="multipart/form-data">
+					<input type="file" name="myimage1"/><br/>
+				<br/>
+					<input type="file" name="myimage2"/><br/>
+				<br/>
+					<input type="submit" name="submit"/>
+						
+					</form>
+				</div>
 
+				<br/>
+				
 				<Link className="link_to_page_two" to={'/page_two'}>Go to page 2</Link>
 			</div>
 		);
--- webpack-dev-server.js
+++ webpack-dev-server.js
@@ -7,6 +7,37 @@ const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const webpackConfig = require('./webpack.config'); // haven't created this yet. No sweat.
 const path = require('path');
+const express = require('express');
+const app = express();
+const proxy = require('proxy-middleware');
+const url = require('url');
+const router = express.Router();
+const multer = require('multer');
+const storage = multer.diskStorage({
+	destination: function(req, file, cb){
+		cb(null,'imgs')
+	},
+	filename: function(req, file, cb){
+		cb(null, Date.now() + file.originalname);
+	}
+});
+
+const upload = multer({storage:storage});
+
+app.get('/',function(req, res, next){
+	res.render('index',{title: 'Express'});
+});
+
+app.post('/',upload.any(),function(req,res,next){
+	console.log(req.files);
+	res.send(req.files);
+});
+/*
+app.post('/', function (req, res) {
+      res.sendFile(path.join(__dirname +'/jose.html'));
+});*/
+
+//app.use('/*', proxy(url.parse('http://localhost:3000/assets')));
 
 // always dev enviroment when running webpack dev server
 const env = { dev: process.env.NODE_ENV };
@@ -25,4 +56,5 @@ const devServerConfig = {
 const server = new WebpackDevServer(webpack(webpackConfig(env)), devServerConfig);
 
 // will be live at http://localhost:3000/
-server.listen(3000, 'localhost');
+server.listen(3000, '0.0.0.0');
+app.listen(3001);
